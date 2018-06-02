a8d3c5199d561bd74a7970291bb13fd2cfacfb21
creemb <creemb@wit.edu>
2018-06-01 23:35:31 -0400

    Removed unnecessary function headers

--- mm-header.h
+++ mm-header.h
@@ -1,14 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define BensType double
+#define BensType int
 
 BensType** matMultiply(BensType** mat1, BensType** mat2, int n);
-int** matMultiplyInt(int **mat1, int **mat2, int n);
 void freeMat(BensType** mat, int n);
-void freeMatInt(int** mat, int n);
 double **allocMat(BensType** mat, int n);
-int **allocMatInt(int **mat, int n);
 void assignMat(BensType** mat, int n);
-void assignMatInt(int**mat, int n);
 void printResult(BensType** mat1, BensType** mat2, BensType** r, int n);
 void printMat(BensType** mat, int n);
